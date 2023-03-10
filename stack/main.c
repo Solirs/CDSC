3c69c1212ea3e01b3d04419a3852aaf2f6d81770
Solirs <zcauchemar@gmail.com>
2023-03-11 00:56:09 +0100

    Skeleton structure for stack implementation

---
+++ stack/main.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include "stack.h"
+
+int main(){
+
+    return 0;
+
+}
---
+++ stack/stack.h
@@ -0,0 +1,10 @@
+#ifndef STACK_
+#define STACK_
+
+#include<stdlib.h>
+
+struct stack{
+    int size;
+    void* content;
+};
+#endif
