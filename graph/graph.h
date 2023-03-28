1bdb788e35228a837fc6c28e729cb7088317afc3
lukakralik <luka.kralik@gmail.com>
2023-03-28 16:21:57 +0200

    init, len and is_empty functions done (without memory checking)

--- graph/graph.h
+++ graph/graph.h
@@ -4,24 +4,19 @@
 #include <stdbool.h>
 #include <string.h>
 
-struct graph_node {
-	graph_node *neighboirs;
-	void *value;
-};
-
 struct graph {
-	graph_node *nodes;
-	size_t len;
+	void **adj_matrix;
+	int nodes;
 };
 
-struct graph cdsc_graph_init(void);
-void *cdsc_graph_nuke(graph *g);
+struct graph *cdsc_graph_init(size_t nodes);
+void *cdsc_graph_nuke(struct graph *g);
 
-bool cdsc_graph_insert(graph *g, void *node1, void *node2);
-void *cdsc_graph_delete(graph *g, void *node);
-bool cdsc_graph_search(graph *g, void *node);
+bool cdsc_graph_insert(struct graph *g, void *node1, void *node2);
+void *cdsc_graph_delete(struct graph *g, void *node);
+bool cdsc_graph_search(struct graph *g, void* node1, void* node2);
 
-size_t cdsc_graph_len(graph *g);
-bool cdsc_graph_is_emtpy(graph *g)
+size_t cdsc_graph_len(struct graph *g);
+bool cdsc_graph_is_emtpy(struct graph *g);
 
 #endif
