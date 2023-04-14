fbdca1cd0910059ba9135812792ee7c496327849
Solirs <zcauchemar@gmail.com>
2023-04-14 15:45:21 +0200

    LINKEDLIST: Apply naming scheme

--- linkedlist/linkedlist.c
+++ linkedlist/linkedlist.c
@@ -1,8 +1,8 @@
 #include "linkedlist.h"
 
-// Add new node at the head, replacing it.
-void inserthead(struct LinkedList *list, void* data){
-    struct node* ins = malloc(sizeof(struct node));
+// Add new cdsc_linkedlist_node at the head, replacing it.
+void cdsc_linkedlist_inserthead(struct cdsc_linkedlist *list, void* data){
+    struct cdsc_linkedlist_node* ins = malloc(sizeof(struct cdsc_linkedlist_node));
     ins->data = data;
     if (list->size == 0){
         ins->next = NULL;
@@ -15,9 +15,9 @@ void inserthead(struct LinkedList *list, void* data){
     list->size++;
 }
 
-// Add new node at the tail, replacing it.
-void inserttail(struct LinkedList *list, void* data){
-    struct node* ins = malloc(sizeof(struct node));
+// Add new cdsc_linkedlist_node at the tail, replacing it.
+void cdsc_linkedlist_inserttail(struct cdsc_linkedlist *list, void* data){
+    struct cdsc_linkedlist_node* ins = malloc(sizeof(struct cdsc_linkedlist_node));
     ins->data = data;
     ins->next = NULL;
     if (list->size == 0){
@@ -32,15 +32,15 @@ void inserttail(struct LinkedList *list, void* data){
 
 }
 
-// Remove the list's tail replacing it by the node before it
-void* poptail(struct LinkedList *list){
-    struct node *newtail;
+// Remove the list's tail replacing it by the cdsc_linkedlist_node before it
+void* cdsc_linkedlist_poptail(struct cdsc_linkedlist *list){
+    struct cdsc_linkedlist_node *newtail;
     int i;
     for (i = 0;i<list->size; i++){
         newtail = newtail->next;
     }
     void* ret = list->tail->data;
-    //We need to free the node struct that was allocated in inserttail or inserthead
+    //We need to free the cdsc_linkedlist_node struct that was allocated in inserttail or inserthead
     free(list->tail);
     list->tail = NULL;
     if (list->size > 1){
@@ -51,11 +51,11 @@ void* poptail(struct LinkedList *list){
     return ret;
 
 }
-// Remove the list's head replacing it with the next node
-void* pophead(struct LinkedList *list){
-    struct node *newhead = list->head->next;
+// Remove the list's head replacing it with the next cdsc_linkedlist_node
+void* cdsc_linkedlist_pophead(struct cdsc_linkedlist *list){
+    struct cdsc_linkedlist_node *newhead = list->head->next;
     void* ret = list->head->data;
-    //We need to free the node struct that was allocated in inserttail or inserthead
+    //We need to free the cdsc_linkedlist_node struct that was allocated in inserttail or inserthead
     free(list->head);
     list->head = NULL;
 
@@ -68,25 +68,30 @@ void* pophead(struct LinkedList *list){
 }
 
 // Get data by index starting at the heazd
-void* getindexfromhead(struct LinkedList *list, int index){
-
+void* cdsc_linkedlist_getindexfromhead(struct cdsc_linkedlist *list, int index){
+	if (list->head == NULL){
+		return NULL;
+	}
     int i;
-    struct node* cur = list->head;
+    struct cdsc_linkedlist_node* cur = list->head;
+    
 
     for (i = 0; i<index; i++){
         cur = cur->next;
+
         if (cur == NULL){
             return NULL;
         }
+
     }
 
     return cur->data;
 }
 
 // Get data by index starting at the tail
-void* getindexfromtail(struct LinkedList *list, int index){
+void* cdsc_linkedlist_getindexfromtail(struct cdsc_linkedlist *list, int index){
     int i;
-    struct node* cur = list->head;
+    struct cdsc_linkedlist_node* cur = list->head;
     if (index == 0){
         return list->tail->data;
     }
@@ -107,8 +112,8 @@ void* getindexfromtail(struct LinkedList *list, int index){
 }
 
 // Create and init double linked list
-struct LinkedList *make_ll(){
-    struct LinkedList *ret = malloc(sizeof(struct LinkedList));
+struct cdsc_linkedlist *cdsc_linkedlist_make_ll(){
+    struct cdsc_linkedlist *ret = malloc(sizeof(struct cdsc_linkedlist));
     ret->size = 0;
     ret->head = NULL;
     ret->tail = NULL;
@@ -116,8 +121,8 @@ struct LinkedList *make_ll(){
 }
 
 // Completely wipe a doubly linked list
-void nuke(struct LinkedList *list){
-    struct node *cur;
+void cdsc_linkedlist_nuke(struct cdsc_linkedlist *list){
+    struct cdsc_linkedlist_node *cur;
     if (list->size == 0){
         return NULL;
     }
@@ -127,7 +132,7 @@ void nuke(struct LinkedList *list){
     }else{
         cur = list->head;
         while(1){
-            struct node *nxt = cur->next;
+            struct cdsc_linkedlist_node *nxt = cur->next;
             if (cur == list->tail){
                 free(cur);
                 cur = NULL;
@@ -144,9 +149,9 @@ void nuke(struct LinkedList *list){
 }
 
 // Sets the data of a DoublyLinkedList's certain index, starting from the head.
-void setdata(struct LinkedList *list, void* data, int index){
+void cdsc_linkedlist_setdata(struct cdsc_linkedlist *list, void* data, int index){
     int i;
-    struct node* cur = list->head;
+    struct cdsc_linkedlist_node* cur = list->head;
     for (i = 0; i<index; i++){
         cur = cur->next;
         if (cur == NULL){
@@ -157,8 +162,8 @@ void setdata(struct LinkedList *list, void* data, int index){
 
 }
 
-struct node* find(struct LinkedList *list, void* key) {
-	struct node* current = list->head;
+struct cdsc_linkedlist_node* cdsc_linkedlist_find(struct cdsc_linkedlist *list, void* key) {
+	struct cdsc_linkedlist_node* current = list->head;
 	if (list->head == NULL) {
 		return NULL;
 	}
@@ -173,8 +178,8 @@ struct node* find(struct LinkedList *list, void* key) {
 	return current;
 }
 
-int findindex(struct LinkedList *list, void* key) {
-	struct node* current = list->head;
+int cdsc_linkedlist_findindex(struct cdsc_linkedlist *list, void* key) {
+	struct cdsc_linkedlist_node* current = list->head;
     int i = 0;
 	if (list->head == NULL) {
 		return NULL;
@@ -191,15 +196,15 @@ int findindex(struct LinkedList *list, void* key) {
 	return i;
 }
 
-bool is_empty(struct LinkedList *list) {
+bool is_empty(struct cdsc_linkedlist *list) {
 	return list->head == NULL;
 }
-void reverse(struct LinkedList *list) { 
-	struct node* prev = NULL;
-    struct node* formerhead = list->head;
+void cdsc_linkedlist_reverse(struct cdsc_linkedlist *list) { 
+	struct cdsc_linkedlist_node* prev = NULL;
+    struct cdsc_linkedlist_node* formerhead = list->head;
     list->tail = formerhead;
-	struct node* current = list->head;
-	struct node* next;
+	struct cdsc_linkedlist_node* current = list->head;
+	struct cdsc_linkedlist_node* next;
  
 	while (current != NULL) {
 		next = current->next;
@@ -210,14 +215,14 @@ void reverse(struct LinkedList *list) {
 	list->head = prev;
 }
 
-void remove_node_if_contains(struct LinkedList *list, void* key){
+void cdsc_linkedlist_remove_node_if_contains(struct cdsc_linkedlist *list, void* key){
     int i;
-    struct node* cur = list->head;
-    struct node* prev = NULL;
+    struct cdsc_linkedlist_node* cur = list->head;
+    struct cdsc_linkedlist_node* prev = NULL;
     for (i = 0; i<list->size; i++){
         if (cur->data == key){
             if (cur == list->head){
-                pophead(list);
+                cdsc_linkedlist_pophead(list);
             }else{
                 prev->next = cur->next;
                 free(cur);
@@ -231,25 +236,25 @@ void remove_node_if_contains(struct LinkedList *list, void* key){
     cur = NULL;
 }
 
-void appendnode(struct LinkedList *list, struct node* node){
+void cdsc_linkedlist_appendnode(struct cdsc_linkedlist *list, struct cdsc_linkedlist_node* cdsc_linkedlist_node){
     if (list->size == 0){
-        list->head = node;
-        list->tail = node;
+        list->head = cdsc_linkedlist_node;
+        list->tail = cdsc_linkedlist_node;
     }else{
-        list->tail->next = node;
-        list->tail = node;
+        list->tail->next = cdsc_linkedlist_node;
+        list->tail = cdsc_linkedlist_node;
     }
 
 }
 
-void cdsc_linkedlist_merge(struct LinkedList *list1, struct LinkedList *list2){
+void cdsc_linkedlist_merge(struct cdsc_linkedlist *list1, struct cdsc_linkedlist *list2){
     list1->tail->next = list2->head;
     list1->size += list2->size;
 }
 
-bool contains(struct LinkedList* list, void* data){
+bool cdsc_linkedlist_contains(struct cdsc_linkedlist* list, void* data){
     int i;
-    struct node* cur = list->head;
+    struct cdsc_linkedlist_node* cur = list->head;
     for (i = 0; i<list->size; i++){
         if (cur->data == data){
 			return true;
@@ -261,9 +266,9 @@ bool contains(struct LinkedList* list, void* data){
     return false;
 }
 
-// Run a function for each member of a linkedlist.
-void cdsc_linkedlist_foreach(struct LinkedList *list, void (*action)(), void* param){
-		struct node* cur = list->head;
+// Run a function for each member of a cdsc_linkedlist.
+void cdsc_linkedlist_foreach(struct cdsc_linkedlist *list, void (*action)(), void* param){
+		struct cdsc_linkedlist_node* cur = list->head;
 		while (cur != NULL){
 			action(cur, param);
 			cur = cur->next;
