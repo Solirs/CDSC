dfc66f6eab7fd777c13c814c2c85c8932bce0b55
Solirs <zcauchemar@gmail.com>
2023-04-14 15:46:46 +0200

    STACK: Apply linkedlist naming scheme to functions

--- stack/stack.c
+++ stack/stack.c
@@ -4,28 +4,28 @@
 struct cdsc_stack* cdsc_stack_make_stack(){
     struct cdsc_stack* st = malloc(sizeof(struct cdsc_stack));
     st->size = 0;
-    st->content = make_ll();
+    st->content = cdsc_linkedlist_make_ll();
     return st;
 }
 
 void cdsc_stack_push(struct cdsc_stack* stack, void* data){
-    inserthead(stack->content, data);
+    cdsc_linkedlist_inserthead(stack->content, data);
     stack->size++;
 
 }
 
 void* cdsc_stack_pop(struct cdsc_stack* stack){
-    void* ret = pophead(stack->content);
+    void* ret = cdsc_linkedlist_pophead(stack->content);
     stack->size--;
     return ret;
 }
 void* cdsc_stack_peek(struct cdsc_stack* stack){
-    void* ret = getindexfromhead(stack->content, 0);
+    void* ret = cdsc_linkedlist_getindexfromhead(stack->content, 0);
     return ret;
 }
 
 void cdsc_stack_nuke(struct cdsc_stack* stack){
-	nuke(stack->content);
+	cdsc_linkedlist_nuke(stack->content);
 	free(stack->content);
 	stack->size = 0;
 }
