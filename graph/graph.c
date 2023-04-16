d8467d08a52ab2781ced05df5b976eb920596c05
lukakralik <luka.kralik@gmail.com>
2023-04-16 20:11:46 +0200

    GRAPH: structure rework with nodes, adjacency list representation

--- graph/graph.c
+++ graph/graph.c
@@ -3,27 +3,60 @@
 #include<stdbool.h>
 #include "graph.h"
 
-struct graph *cdsc_graph_init(size_t nodes) {
-	struct graph *g = malloc(sizeof(struct graph));
-	g->adj_matrix = calloc(sizeof(bool *), g->nodes);
-	// add some memory checks
+// Internal function for node initialization
+node *cdcsc_node_init(int dest) {
+	node *new_node = (node *) malloc(sizeof(node));
+	new_node->next = NULL;
+	new_node->dest = dest;
+	return new_node;
+}
+
+// Constructor for graph with adjacency list
+graph *cdsc_graph_init(size_t nodes) {
+	graph *g = (graph *) malloc(sizeof(graph));
+	g->nodes = nodes;
+	g->adj_list = (node **) malloc(nodes * sizeof(node *));
 	int i;
-	for(i = 0; i < g->nodes; i++) {
-		g->adj_matrix[i] = calloc(sizeof(bool), g->nodes);
+	for(i = 0; i < nodes; i++) {
+		g->adj_list[i] = NULL;
 	}
 	return g;
 }
-void *cdsc_graph_nuke(struct graph *g);
 
-bool cdsc_graph_insert(struct graph *g, void *node1, void *node2);
-void *cdsc_graph_delete(struct graph *g, void *node);
-bool cdsc_graph_search(struct graph *g, void* node1, void* node2);
+// Adds and undirected edge between two nodes
+void cdsc_graph_insert(graph *g, int source, int target) {
+	// Create a new node and connect it to the graph
+	node *new_node = cdcsc_node_init(target);
+	new_node->next = g->adj_list[source];
+	g->adj_list[source] = new_node;
 
-size_t cdsc_graph_len(struct graph *g) {
+	// Two-way connection
+	new_node = cdcsc_node_init(source);
+	new_node->next = g->adj_list[target];
+	g->adj_list[target] = new_node;
+}
+
+void cdsc_graph_nuke(graph *g) {
+	for (int i = 0; i < g->nodes; i++) {
+		node *current = g->adj_list[i];
+		while (current != NULL) {
+			node *next = current->next;
+			free(current);
+			current = next;	
+		}
+	}
+	free(g->adj_list);
+	free(g);
+}
+
+void *cdsc_graph_delete(graph *g, void *node);
+bool cdsc_graph_search(graph *g, void* node1, void* node2);
+
+size_t cdsc_graph_len(graph *g) {
 	return g->nodes;
 }
 
-bool cdsc_graph_is_emtpy(struct graph *g) {
+bool cdsc_graph_is_emtpy(graph *g) {
 	if (!g->nodes) {
 		return true;
 	}
