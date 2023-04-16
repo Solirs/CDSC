68429a79e2c95bb1804a8ec34822c2fc604e2536
Solirs <zcauchemar@gmail.com>
2023-04-16 19:50:14 +0200

    STACK: Fix warnings and add merge function

--- stack/stack.h
+++ stack/stack.h
@@ -15,4 +15,5 @@ void cdsc_stack_push(struct cdsc_stack* cdsc_stack, void* data);
 void* cdsc_stack_pop(struct cdsc_stack* cdsc_stack);
 void* cdsc_stack_peek(struct cdsc_stack* cdsc_stack);
 void cdsc_stack_nuke(struct cdsc_stack* stack);
+struct cdsc_stack* cdsc_stack_merge(struct cdsc_stack* stack1, struct cdsc_stack* stack2);
 #endif
