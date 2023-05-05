f10c7172729abb856d5d1fcd665698c986e105b4
Solirs <zcauchemar@gmail.com>
2023-05-05 22:10:35 +0200

    QUEUE: Adapt to doublylinkedlist

--- queue/queue.c
+++ queue/queue.c
@@ -6,7 +6,7 @@ struct cdsc_queue *cdsc_queue_make_queue() {
     struct cdsc_queue *ret = malloc(sizeof(struct cdsc_queue));
     ret->capacity = 0;		// Capacity to 0 = infinite
     ret->size = 0;
-    ret->list = make_ll();
+    ret->list = cdsc_doublylinkedlist_make_dll();
     return ret;
 }
 
@@ -14,7 +14,7 @@ void cdsc_queue_enqueue(struct cdsc_queue *queue, void *data) {
     if (queue->size == queue->capacity && queue->capacity != 0) {
 	return NULL;
     } else {
-	inserttail(queue->list, data);
+	cdsc_doublylinkedlist_inserttail(queue->list, data);
 	queue->size++;
     }
 }
@@ -23,8 +23,8 @@ void *cdsc_queue_dequeue(struct cdsc_queue *queue) {
     if (QUEUE_EMPTY) {
 	return NULL;
     } else {
-	void *data = getindexfromhead(queue->list, 0);
-	pophead(queue->list);
+	void *data = cdsc_doublylinkedlist_getindexfromhead(queue->list, 0);
+	cdsc_doublylinkedlist_pophead(queue->list);
 	return data;
     }
 
@@ -34,7 +34,7 @@ void cdsc_queue_nuke(struct cdsc_queue *queue) {
     if (QUEUE_EMPTY) {
 	return NULL;
     }
-    nuke(queue->list);
+    cdsc_doublylinkedlist_nuke(queue->list);
     free(queue->list);
     queue->list = NULL;
     queue->size = 0;
@@ -44,7 +44,7 @@ void *cdsc_queue_getfront(struct cdsc_queue *queue) {
     if (QUEUE_EMPTY) {
 	return NULL;
     }
-    void *data = getindexfromhead(queue->list, 0);
+    void *data = cdsc_doublylinkedlist_getindexfromhead(queue->list, 0);
     return data;
 }
 
@@ -52,7 +52,7 @@ void *cdsc_queue_getrear(struct cdsc_queue *queue) {
     if (QUEUE_EMPTY) {
 	return NULL;
     }
-    void *data = getindexfromtail(queue->list, 0);
+    void *data = cdsc_doublylinkedlist_getindexfromtail(queue->list, 0);
     return data;
 }
 
