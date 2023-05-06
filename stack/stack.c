522c5fc6870051581e5c05835bd6516381a135bf
Solirs <zcauchemar@gmail.com>
2023-05-06 21:46:32 +0200

    STACK: Fix warnings and apply return scheme

--- stack/stack.c
+++ stack/stack.c
@@ -4,15 +4,21 @@
 // Create and init stack
 struct cdsc_stack *cdsc_stack_make_stack() {
     struct cdsc_stack *st = malloc(sizeof(struct cdsc_stack));
+    if (st == NULL){
+		return NULL;
+	}
     st->size = 0;
     st->content = cdsc_linkedlist_make_ll();
     return st;
 }
 
 // Push to the head of stack
-void cdsc_stack_push(struct cdsc_stack *stack, void *data) {
-    cdsc_linkedlist_inserthead(stack->content, data);
+int cdsc_stack_push(struct cdsc_stack *stack, void *data) {
+    if (cdsc_linkedlist_inserthead(stack->content, data) < 0){
+		return -1;
+	}
     stack->size++;
+    return 1;
 
 }
 
@@ -34,16 +40,25 @@ void *cdsc_stack_peek(struct cdsc_stack *stack) {
 struct cdsc_stack *cdsc_stack_merge(struct cdsc_stack *stack1,
 				    struct cdsc_stack *stack2) {
     struct cdsc_stack *st = malloc(sizeof(struct cdsc_stack));
+    if (st == NULL){
+		return NULL;
+	}
     st->content = cdsc_linkedlist_merge(stack1->content, stack2->content);
+    if (st->content == NULL){
+		return NULL;
+	}
     st->size = st->content->size;
     return st;
 }
 
 
 // Zero stack
-void cdsc_stack_nuke(struct cdsc_stack *stack) {
-    cdsc_linkedlist_nuke(stack->content);
+int cdsc_stack_nuke(struct cdsc_stack *stack) {
+    if (cdsc_linkedlist_nuke(stack->content) < 0){
+		return -1;
+	}
     free(stack->content);
 
     stack->size = 0;
+    return 1;
 }
--- stack/stack.h
+++ stack/stack.h
@@ -11,9 +11,9 @@ struct cdsc_stack{
 };
 
 struct cdsc_stack* cdsc_stack_make_stack();
-void cdsc_stack_push(struct cdsc_stack* cdsc_stack, void* data);
+int cdsc_stack_push(struct cdsc_stack* cdsc_stack, void* data);
 void* cdsc_stack_pop(struct cdsc_stack* cdsc_stack);
 void* cdsc_stack_peek(struct cdsc_stack* cdsc_stack);
-void cdsc_stack_nuke(struct cdsc_stack* stack);
+int cdsc_stack_nuke(struct cdsc_stack* stack);
 struct cdsc_stack* cdsc_stack_merge(struct cdsc_stack* stack1, struct cdsc_stack* stack2);
 #endif
