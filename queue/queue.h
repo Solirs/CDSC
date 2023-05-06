917d0886efec34d81783ee60a2849348b8ad885f
Solirs <zcauchemar@gmail.com>
2023-05-06 21:41:35 +0200

    QUEUE: Apply return scheme and fix warnings

--- queue/queue.h
+++ queue/queue.h
@@ -7,16 +7,16 @@
 #define QUEUE_EMPTY queue->size == 0
 #define QUEUE_FULL queue->size == queue->capacity && queue->capacity != 0
 struct cdsc_queue{
-    struct DoubleLinkedList *list;
+    struct cdsc_doublylinkedlist *list;
     int capacity;
     int size;
 };
 
 struct cdsc_queue *make_queue();
-void cdsc_queue_enqueue(struct cdsc_queue *queue, void* data);
+int cdsc_queue_enqueue(struct cdsc_queue *queue, void* data);
 void* cdsc_queue_dequeue(struct cdsc_queue *queue);
 
-void cdsc_queue_nuke(struct cdsc_queue* queue);
+int cdsc_queue_nuke(struct cdsc_queue* queue);
 void* cdsc_queue_getfront(struct cdsc_queue *queue);
 void* cdsc_queue_getrear(struct cdsc_queue *queue);
 int cdsc_queue_is_empty(struct cdsc_queue *queue);
