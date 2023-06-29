fcd76f60a3f70c7803eab84bd8746b8c8dda0f8a
Luka Králík <luka.kralik@gmail.com>
2023-06-29 23:29:55 +0200

    add UF destructor

--- unionfind/unionfind.c
+++ unionfind/unionfind.c
@@ -7,4 +7,8 @@ void cdsc_union_find_uni(union_find *set, int a, int b);
 bool cdsc_union_find_same(union_find *set, int a, int b);
 
 union_find *cdsc_union_find_init(int a);
-void nuke(union_find *set);
+
+void nuke(union_find *set) {
+	free(set->par);
+	free(set->rank);
+}
