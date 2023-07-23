b0d4f823c81dda549007e663fc227f7e54663425
Solirs <zcauchemar@gmail.com>
2023-07-23 18:37:29 +0200

    Remove cdsc_bitarray_create

--- bitarray/bitarray.c
+++ bitarray/bitarray.c
@@ -1,10 +1,5 @@
 #include "bitarray.h"
 
-int32_t* cdsc_bitarray_create(int size){
-    int array[size/32];
-    return array;
-}
-
 void cdsc_bitarray_set1(int32_t array[], int bit){
     int index = bit/32;
     int pos = bit%32;
