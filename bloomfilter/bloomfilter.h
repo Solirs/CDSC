6e45a2b5d2244eccbdc986eb8c9d71088451914b
Solirs <zcauchemar@gmail.com>
2023-07-24 01:46:21 +0200

    BLOOMFILTER: Remove hash function pointer parameter

--- bloomfilter/bloomfilter.h
+++ bloomfilter/bloomfilter.h
@@ -25,5 +25,5 @@ struct cdsc_bloomfilter* cdsc_bloomfilter_create(int size);
 void cdsc_bloomfilter_nuke(struct cdsc_bloomfilter* bf);
 void cdsc_bloomfilter_add(struct cdsc_bloomfilter* bf, void* data);
 int cdsc_bloomfilter_check(struct cdsc_bloomfilter* bf, void* data);
-void cdsc_bloomfilter_addhashfun(struct cdsc_bloomfilter* bf, unsigned int (*hashfun)(void*));
+void cdsc_bloomfilter_addhashfun(struct cdsc_bloomfilter* bf, unsigned int (*hashfun)());
 #endif
