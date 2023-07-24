30a7af2533beff0eeb920aab7cfc0f4c6559aad8
Solirs <zcauchemar@gmail.com>
2023-07-24 17:46:18 +0200

    BITARRAY: Add comments

--- bitarray/bitarray.c
+++ bitarray/bitarray.c
@@ -1,5 +1,6 @@
 #include "bitarray.h"
 
+//Set the bit at index 'bit' to 1
 void cdsc_bitarray_set1(int32_t array[], int bit){
     int index = bit/32;
     int pos = bit%32;
@@ -11,6 +12,7 @@ void cdsc_bitarray_set1(int32_t array[], int bit){
     array[index] = array[index] | flag;
 }
 
+//Set the bit at index 'bit' to 0
 void cdsc_bitarray_set0(int32_t array[], int bit){
     int index = bit/32;
     int pos = bit%32;
@@ -22,6 +24,7 @@ void cdsc_bitarray_set0(int32_t array[], int bit){
     array[index] = array[index] & ~flag;
 }
 
+// Check if the bit at index 'bit' is 1
 int cdsc_bitarray_is1(int32_t array[], int bit){
     int index = bit/32;
     int pos = bit%32;
@@ -29,6 +32,8 @@ int cdsc_bitarray_is1(int32_t array[], int bit){
     return (array[index] & (1 << pos)) != 0;
 }
 
+
+// Check if the bit at index 'bit' is 0
 int cdsc_bitarray_is0(int32_t array[], int bit){
     int index = bit/32;
     int pos = bit%32;
